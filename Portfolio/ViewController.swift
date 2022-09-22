946165049a3039ef788ea4c2dd60d95bcf737d58
Harsh Raj <harsh.raj@nagarro.com>
2022-09-22 14:34:30 +0530

    Added tableView protocols

--- Portfolio/ViewController.swift
+++ Portfolio/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
@@ -29,3 +32,23 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Tapped")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "skillCell", for: indexPath)
+        cell.textLabel?.text = "Harsh"
+        return cell
+    }
+    
+    
+}
