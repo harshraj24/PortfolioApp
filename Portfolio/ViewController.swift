708c030036acdaa5a8b5d14de2aa46177130dcdc
Harsh Raj <harsh.raj@nagarro.com>
2022-09-23 09:16:48 +0530

    Changed Some Files

--- Portfolio/ViewController.swift
+++ Portfolio/ViewController.swift
@@ -13,20 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var profile: UILabel!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var selfImage: UIImageView!
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var skillTableView: UITableView!
+    @IBOutlet weak var toolTableView: UITableView!
+    
+    let skillSet = ["Swift", "UIKit", "SwiftUI", "Agile (Scrum)", "XCTestCases", "XCUITestCases", "HTML5", "CSS3", "C/C++"]
+    
+    let toolSet = ["Xcode", "JIRA", "SharePoint", "SourceTree", "Postman", "Robo 3T", "Push Notifications"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         
-        tableView.delegate = self
-        tableView.dataSource = self
+        skillTableView.delegate = self
+        skillTableView.dataSource = self
+        toolTableView.delegate = self
+        toolTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
     func setupView() {
         self.name.text = "Harsh Raj"
         self.profile.text = "iOS Developer"
+        skillTableView.backgroundColor = .black
+        toolTableView.backgroundColor = .black
     }
 
 
@@ -41,13 +50,23 @@ extension ViewController: UITableViewDelegate {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return tableView == skillTableView ? skillSet.count : toolSet.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "skillCell", for: indexPath)
-        cell.textLabel?.text = "Harsh"
-        return cell
+        if tableView == skillTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "skillCell", for: indexPath)
+            cell.backgroundColor = .black
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.text = skillSet[indexPath.row]
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "toolCell", for: indexPath)
+            cell.backgroundColor = .black
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.text = toolSet[indexPath.row]
+            return cell
+        }
     }
     
     
